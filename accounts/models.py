64584bfbe19f761000a6f645c6ba1c477220e783
jngcii <concotree@gmail.com>
2020-04-22 21:26:29 +0900

    Update accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Account(models.Model):
+class Account(AbstractUser):
+    
     def __str__(self):
-        return f'{self.first_name}, {self.last_name}'
+        return f'{self.username} - {self.first_name} - {self.last_name}'
