67c93d8b54dbf15124613065d0d6f80ed92cab9e
JHyuk2 <whdgurdl77@gmail.com>
2020-04-22 14:46:43 +0900

    Add models(Account, Review, Comment)

--- community/models.py
+++ community/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from accounts.models import Account
 # Create your models here.
+
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Review(TimeStamp):
+    title = models.CharField(max_length=100)
+    movie_title = models.CharField(max_length=30)
+    rank = models.IntegerField()
+    content = models.TextField()
+    creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='reviews')
+
+class Comment(TimeStamp):
+    content = models.TextField()
+    creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
