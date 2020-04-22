1667a8dd18e8a83d6071c3a53d6ac79c10205f58
JHyuk2 <whdgurdl77@gmail.com>
2020-04-22 14:55:28 +0900

    Migrate

--- community/models.py
+++ community/models.py
@@ -2,18 +2,22 @@ from django.db import models
 from accounts.models import Account
 # Create your models here.
 
-class TimeStamp(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# class TimeStamp(models.Model):
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
-class Review(TimeStamp):
+class Review(models.Model):
     title = models.CharField(max_length=100)
     movie_title = models.CharField(max_length=30)
     rank = models.IntegerField()
     content = models.TextField()
     creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='reviews')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Comment(TimeStamp):
+class Comment(models.Model):
     content = models.TextField()
     creator = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='comments')
     review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
