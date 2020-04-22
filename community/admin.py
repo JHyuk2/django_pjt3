0e8d524da698a594522ab2cd170b38435e0ed4ec
JHyuk2 <whdgurdl77@gmail.com>
2020-04-22 20:34:37 +0900

    Add forms.py & Add ModelAdmin(ReviewAdmin, CommentAdmin)

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Review, Comment
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'created_at', 'updated_at', 'creator')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_dispaly = ('id','review_id', 'creator')
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
