07d667eaa97ba309dd83bc428df11a50b47e9b1c
jngcii <concotree@gmail.com>
2020-04-22 19:28:18 +0900

    Fix typo

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import get_user_model
-from djangn.contrib.auth import login as auth_login
-from djangn.contrib.auth import logout as auth_logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.decorators.http import required_POST
+from django.views.decorators.http import require_POST
 
-from .forms import CustomUserChangeForm
+# from .forms import CustomUserChangeForm
 
 # Create your views here.
 def signup(request):
@@ -14,34 +14,34 @@ def signup(request):
     if request.user.is_authenticated:
         return redirect('community:index')
 
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('community:index')
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('community:index')
     
     # form => 유효하지 않을 때
-    form = UserCreationForm()
+    # form = UserCreationForm()
     context = {
-        'form':form,
+        'form': form,
     }
-    return render(request, 'community/sign_up.html', context)
+    return render(request, 'sign_up.html', context)
 
 def signin(request):
     # 로그인 되어있을 경우
     if request.user.is_authenticated:
         return redirect('community:index')
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            auth_login(request, form.get_uesr())
-            return redirect(request.GET.get('next') or 'community:index')
+    # if request.method == 'POST':
+    #     form = AuthenticationForm(request, request.POST)
+    #     if form.is_valid():
+    #         auth_login(request, form.get_uesr())
+    #         return redirect(request.GET.get('next') or 'community:index')
     # invalid
-    form = AuthenticationForm()
+    # form = AuthenticationForm()
     context = {
-        'form':form,
+        'form': form,
     }
-    return render(request, 'community/sign_in.html', context)
+    return render(request, 'sign_in.html', context)
 
 @login_required
 def logout(request):
